ad449e9165be3d5d3e10554ac543259295522b71
Mario Fischer <klicker-mail@gmx.net>
2018-04-15 19:37:33 +0200

    Remove semicolon from end of line

--- Webserver/server.lua
+++ Webserver/server.lua
@@ -19,10 +19,10 @@ gpio.mode(LED2, gpio.OUTPUT)
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
     conn:on("receive", function(client,request)
-        local buf = "";
-        local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
+        local buf = ""
+        local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP")
         if(method == nil)then
-            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP");
+            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP")
         end
         local _GET = {}
         if (vars ~= nil)then
@@ -30,21 +30,21 @@ srv:listen(80,function(conn)
                 _GET[k] = v
             end
         end
-        buf = buf.."<h1>NodeMCU TCP - Web Server</h1>";
-        buf = buf.."<p>LED 1 - LED Connected to D1 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
-        buf = buf.."<p>LED 2 - Onboard Blue <a href=\"?pin=ON2\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF2\"><button>OFF</button></a></p>";
+        buf = buf.."<h1>NodeMCU TCP - Web Server</h1>"
+        buf = buf.."<p>LED 1 - LED Connected to D1 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>"
+        buf = buf.."<p>LED 2 - Onboard Blue <a href=\"?pin=ON2\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF2\"><button>OFF</button></a></p>"
         local _on,_off = "",""
         if(_GET.pin == "ON1")then
-              gpio.write(LED1, gpio.HIGH);
+              gpio.write(LED1, gpio.HIGH)
         elseif(_GET.pin == "OFF1")then
-              gpio.write(LED1, gpio.LOW);
+              gpio.write(LED1, gpio.LOW)
         elseif(_GET.pin == "ON2")then
-              gpio.write(LED2, gpio.LOW);
+              gpio.write(LED2, gpio.LOW)
         elseif(_GET.pin == "OFF2")then
-              gpio.write(LED2, gpio.HIGH);
+              gpio.write(LED2, gpio.HIGH)
         end
-        client:send(buf);
-        client:close();
-        collectgarbage();
+        client:send(buf)
+        client:close()
+        collectgarbage()
     end)
 end)
